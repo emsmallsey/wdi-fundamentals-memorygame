a2f4ceb186cf349414d1fd7404ee626374f7841d
emsmallsey <emilysmall222@gmail.com>
2020-05-04 09:11:35 +1000

    worked on the functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -5,23 +5,24 @@ const cards = ["queen", "queen", "king", "king"];
 
 const cardsInPlay = [];
 
-let cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log("User flipped queen");
-
-let cardTwo = cards[1];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped King");
-
-if (cardsInPlay.length === 2) {
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-        alert("It's a match!");
-    }
-    else {
-        alert("Please pack your things and leave the house immediately. It's not a match.")
+function checkMatch() {
+    if (cardsInPlay.length === 2) {
+        if (cardsInPlay[0] === cardsInPlay[1]) {
+            alert("It's a match!");
+        }
+        else {
+            alert("Please pack your things and leave the house immediately. It's not a match.")
+        }
     }
 }
+
+function flipCard(cardID) {
+    console.log("User flipped " + cards[cardID]);
+    cardsInPlay.push(cards[cardID]);
+    checkMatch();
+}
+
+
+flipCard(0);
+flipCard(2);
+
