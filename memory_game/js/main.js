285671c8d73da85bf6d684ad8c774c7bc84f5b06
emsmallsey <emilysmall222@gmail.com>
2020-05-04 08:04:19 +1000

    created game loops

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,27 @@
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "King";
-let cardFour = "king";
+// console.log("User flipped " + cardOne);
+// console.log ("User flipped " + cardThree);
 
-console.log("User flipped " + cardOne);
-console.log ("User flipped " + cardThree);
+const cards = ["queen", "queen", "king", "king"];
+
+const cardsInPlay = [];
+
+let cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+console.log("User flipped queen");
+
+let cardTwo = cards[1];
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped King");
+
+if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("It's a match!");
+    }
+    else {
+        alert("Please pack your things and leave the house immediately. It's not a match.")
+    }
+}
