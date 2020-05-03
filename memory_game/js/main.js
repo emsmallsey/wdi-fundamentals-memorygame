3a873b76988cdc4afd35ca57442be2908b1db7cc
emsmallsey <emilysmall222@gmail.com>
2020-05-03 21:06:15 +1000

    created game html, css and logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+let cardOne = "queen";
+let cardTwo = "queen";
+let cardThree = "King";
+let cardFour = "king";
+
+console.log("User flipped " + cardOne);
+console.log ("User flipped " + cardThree);
